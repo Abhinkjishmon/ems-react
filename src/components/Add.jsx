24aab0a25f0c5fbb08a17ab252243d725befd1f8
Abhinkjishmon <abhinkjishmon001@gmail.com>
2023-03-12 23:13:33 +0530

    new employee add section started

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 const Add = () => {
   return (
-    <div>Add</div>
+    <div>
+      <div className="container-fluid mt-5">
+        <h1 ><i className='fa-solid fa-user-plus me-2'></i>New Employee Adding Form
+          
+        </h1>
+        <p style={{ textAlign: 'justify' }}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus,
+          ea officia. Aut laboriosam ipsa, alias debitis facere quasi iusto
+          ut consequuntur sequi laudantium adipisci placeat, quod beatae fugit
+          odit consectetur ea impedit doloribus quidem quae ducimus! Molestiae
+          doloremque nam recusandae, et praesentium, nostrum, debitis odio ducimus
+          beatae officiis quidem ex.Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus,
+          ea officia. Aut laboriosam ipsa, alias debitis facere quasi iusto
+          ut consequuntur sequi laudantium adipisci placeat, quod beatae fugit
+          odit consectetur ea impedit doloribus quidem quae ducimus! Molestiae
+          doloremque nam recusandae, et praesentium, nostrum, debitis odio ducimus
+          beatae officiis quidem ex.
+        </p>
+      </div>
+    </div>
   )
 }
 
